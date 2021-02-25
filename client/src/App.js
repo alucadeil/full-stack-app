d4e4e2e822a4eae396d03ad3316ad34f3a561b0f
alucadeil <alucadeil@mail.ru>
2021-02-25 13:32:28 +0300

    Initial, simple auth finished

---
+++ client/src/App.js
@@ -0,0 +1,26 @@
+import {BrowserRouter as Router} from 'react-router-dom'
+import {useRoutes} from './routes'
+import {useAuth} from './hooks/auth.hook'
+import 'materialize-css'
+import {AuthContext} from './context/AuthContext'
+import {Navbar} from './components/Navbar'
+
+function App() {
+  const {token, login, logout, userId} = useAuth()
+  const isAuthenticated = !!token
+  const routes = useRoutes(isAuthenticated)
+  return (
+    <AuthContext.Provider value = {{
+      token, userId, login, logout, isAuthenticated
+    }}>
+      <Router>
+        {isAuthenticated && <Navbar />}
+        <div className="container">
+          {routes}
+        </div>
+      </Router>
+    </AuthContext.Provider>
+  );
+}
+
+export default App
